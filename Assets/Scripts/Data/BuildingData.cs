3d2978d5b9828c8c579c6594610db0f0af8136f4
Ajw <Theownage79@gmail.com>
2026-05-08 13:03:29 -0600

    initalCommit

---
+++ Assets/Scripts/Data/BuildingData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "New Building", menuName = "Construction/Building")]
+public class BuildingData : ScriptableObject
+{
+    public string buildingName;
+    public TileBase[] rotatedTiles; // 0:Right, 1:Down, 2:Left, 3:Up
+    public bool isConveyor;
+}
