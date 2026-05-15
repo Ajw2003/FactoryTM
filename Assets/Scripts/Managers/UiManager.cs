5288a9f8f2fc2fccab784c1b4059f00de79c64b1
Ajw <Theownage79@gmail.com>
2026-05-15 13:11:36 -0600

    Tooltips, cost changes and sprites

--- Assets/Scripts/Managers/UiManager.cs
+++ Assets/Scripts/Managers/UiManager.cs
@@ -5,9 +5,57 @@ using UnityEngine;
 public class UiManager : SingletonBase<UiManager>
 {
    public TMP_Text currentCurrency;
+   
+   [Header("Tooltip")]
+   public GameObject tooltipPanel;
+   public TMP_Text tooltipText;
+   private RectTransform tooltipRect;
+
+   protected override void Awake()
+   {
+      base.Awake();
+      if (tooltipPanel != null)
+      {
+         tooltipRect = tooltipPanel.GetComponent<RectTransform>();
+         tooltipPanel.SetActive(false);
+      }
+   }
+
+   private void Update()
+   {
+      if (tooltipPanel != null && tooltipPanel.activeSelf)
+      {
+         UpdateTooltipPosition();
+      }
+   }
+
+   private void UpdateTooltipPosition()
+   {
+      Vector2 mousePos = Input.mousePosition;
+      // Offset to avoid being directly under the cursor (prevents flickering)
+      tooltipRect.position = mousePos + new Vector2(15, -15);
+   }
 
    public void UpdateCurrency(float value)
    {
       currentCurrency.text = value.ToString();
+   }
+
+   public void ShowTooltip(string description)
+   {
+      if (tooltipPanel != null && tooltipText != null)
+      {
+         tooltipText.text = description;
+         tooltipPanel.SetActive(true);
+         UpdateTooltipPosition(); // Position it immediately
+      }
+   }
+
+   public void HideTooltip()
+   {
+      if (tooltipPanel != null)
+      {
+         tooltipPanel.SetActive(false);
+      }
    }
 }
