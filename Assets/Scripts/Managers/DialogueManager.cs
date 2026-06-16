4f682e595ba48e31210d2e7f70ed491ef45c8dd0
Ayden <Theownage79@gmail.com>
2026-06-16 14:14:59 -0600

    add DialogueManager and create inital dialogue logic

--- Assets/Scripts/Managers/DialogueManager.cs
+++ Assets/Scripts/Managers/DialogueManager.cs
@@ -1,16 +1,43 @@
+using System;
+using System.Collections;
+using Singleton;
+using TMPro;
 using UnityEngine;
 
-public class DialogueManager : MonoBehaviour
+public class DialogueManager : SingletonBase<DialogueManager>
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public DialogueSO currentDialogue;
+    public int currentDialogueIndex;
+    public float delayBetweenCharacters;
+    public string currentDialogueText;
+    
+    [SerializeField] private TMP_Text text;
+
+    private void Start()
     {
+        SetDialogue();
+        StartCoroutine(DialogueCoroutine());
+    }
+
+    private IEnumerator DialogueCoroutine()
+    {
+        foreach (var character in currentDialogueText)
+        {
+            yield return new WaitForSeconds(delayBetweenCharacters);
+            text.text += character;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetDialogue()
     {
-        
+        currentDialogueIndex = 0;
+        currentDialogueText = currentDialogue.dialogues[currentDialogueIndex].text;
+        text.text = null;
+    }
+
+    private void IncrementDialogueIndex()
+    {
+        currentDialogueIndex+= 1 % currentDialogue.dialogues.Length;
     }
 }
