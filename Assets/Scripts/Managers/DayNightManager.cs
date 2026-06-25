00f78cd71ace4aaad7c84bb9c784b1e3f0f27d34
ayden <aydenjwilkins@gmail.com>
2026-06-24 21:32:57 -0600

    fix monogram asset and fix day night trying to get a canvas with the wrong get component

--- Assets/Scripts/Managers/DayNightManager.cs
+++ Assets/Scripts/Managers/DayNightManager.cs
@@ -214,7 +214,7 @@ public class DayNightManager : SingletonBase<DayNightManager>
             Outline outline = timerGo.AddComponent<Outline>();
             outline.effectColor = new Color(0f, 0f, 0f, 0.75f);
             outline.effectDistance = new Vector2(1.5f, -1.5f);
-            Canvas canvas = timerText.GetComponent<Canvas>();
+            Canvas canvas = timerText.GetComponentInParent<Canvas>();
             
             canvas.worldCamera = Camera.main; 
         }
