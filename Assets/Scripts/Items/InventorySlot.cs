25d609807efc3217b9cc14737e68d83243c7b5e2
ayden <aydenjwilkins@gmail.com>
2026-07-20 23:43:34 -0600

    more implementation work with text for name and count functional now

--- Assets/Scripts/Items/InventorySlot.cs
+++ Assets/Scripts/Items/InventorySlot.cs
@@ -1,5 +1,6 @@
 using System;
 using Code.Scripts.EventSystems;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -13,11 +14,24 @@ namespace Items
         public Image _image;
         public bool slotFilled;
         private RectTransform _rectTransform;
+        [SerializeField] private TMP_Text textPrefab;
+        [SerializeField] private Vector2 textSpawnOffset = new Vector2(0, 5);
+        [SerializeField] private float textSpacing = 20f;
+        private TMP_Text _nameText;
+        private TMP_Text _countText;
 
         private void Start()
         {
             _image = GetComponent<Image>();
             _rectTransform = GetComponent<RectTransform>();
+            var textOffset = textSpawnOffset;
+    
+            _nameText = Instantiate(textPrefab, _rectTransform);
+            _nameText.transform.SetParent(_rectTransform);
+            _nameText.rectTransform.anchoredPosition += textOffset;
+            _countText = Instantiate(textPrefab, _rectTransform);
+            _countText.transform.SetParent(_rectTransform);
+            _countText.rectTransform.anchoredPosition += textOffset + new Vector2(0, textSpacing);
         }
 
         public void OnPointerEnter(PointerEventData eventData)
@@ -33,6 +47,12 @@ namespace Items
             float angleRadians = transform.eulerAngles.z * Mathf.Deg2Rad;
             return TwoDCollision.CreateFromRotated(_rectTransform.position.x, _rectTransform.position.y, _rectTransform.sizeDelta.x, _rectTransform.sizeDelta.y, angleRadians);
         }
+
+        private void IncreaseCount()
+        {
+            itemCount++;
+            _countText.text = itemCount.ToString();
+        }
         
         
         public void CheckCollision(Rectangle2D itemBox, Item item)
@@ -44,14 +64,15 @@ namespace Items
             {
                 if (slotFilled)
                 {
-                    if (item.resourceType == _currentItem.resourceType){ itemCount++; Destroy(item.gameObject);}
+                    if (item.resourceType == _currentItem.resourceType) { IncreaseCount(); Destroy(item.gameObject);}
                     else item.ResetPosition();
                 }
                 else
                 {
                     slotFilled = true;
                     _currentItem = item;
-                    itemCount++;
+                    IncreaseCount();
+                    _nameText.text = item.name;
                     _image.sprite = item.itemData.sprite;
                     Destroy(item.gameObject);
                 }
